e08277cff1c6e8e884b648c0f664a49d3333b666
Salbih Mustafa <salbih.mustafa@certegy.com>
2022-04-14 10:46:59 -0400

    Initial commit for template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card, Button} from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    return (
+        <div className="main-container">
+            <div className="content">
+                <Card className="text-center" style={{width: '20rem', margin: '0 auto'}}>
+                    <Card.Body>
+                        <Card.Title>Parent State</Card.Title>
+                        <Button variant="primary">Toggle State</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+        </div>
+    )
 }
 
 export default App;
